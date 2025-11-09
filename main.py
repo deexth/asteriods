56958087b5c10928c6631787a28d931c815cf8c4
deexth <hkabare@gmail.com>
2025-11-09 20:44:40 +0100

    player added

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
 import pygame
-from constants import SCREEN_HEIGHT, SCREEN_WIDTH, ASTEROID_MIN_RADIUS
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 
 def main():
     pygame.init()  # pylint: disable=no-member
+    player = Player(
+        x=(SCREEN_WIDTH / 2),
+        y=(SCREEN_HEIGHT / 2),
+    )
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        player.update(dt)
+
         screen.fill(color="black")
+
+        player.draw(screen)
+
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
